57f772ce28edd352ef0e979ff797803acb6aa7ac
codexjoshy <joshuaebhoriao@gmail.com>
2019-11-01 15:01:27 +0100

    my first commit

---
+++ day1/js/concat.js
@@ -0,0 +1,20 @@
+let name = "Josh";
+let address = "akinrele street";
+let number = 3;
+console.log("My name is" +name+'I live at '+number+' '+address);
+
+console.log(`my name is ${name} I live at ${number} ${address} `);
+
+num1 = "3";
+num2 = 3;
+// console.log(num1 + num2);
+// console.log(num2 + num1);
+console.log(num1 - num2);
+console.log(num2 * num1);
+console.log(num2-num1);
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log("num 1 is a " +typeof num1);
+console.log("num 2 is a " +typeof num2);
+
---
+++ day1/js/scope.js
@@ -0,0 +1,15 @@
+document.write('josh');
+// let 2josh ='name';
+// let @me = 'josh';
+let name = 'josh';
+let otherName = 'Prosper';
+
+name = 'Uche';
+console.log(name);
+//scope
+if(name){
+
+name = 'Mike';
+console.log(name);
+}
+console.log(name);
