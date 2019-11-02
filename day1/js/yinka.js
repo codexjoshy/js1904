d1a954755641135526b43b94222b3245f8d49b65
joeveloper <bebosir@gmail.com>
2019-11-02 01:58:20 +0100

    Added my Bio

---
+++ day1/js/yinka.js
@@ -0,0 +1,10 @@
+let myName = "<p>My name is Yinka but my guys call me Joeveloper<p/>";
+let myAge = "I am 20 years old";
+let myAddy =
+  "<p>I work at Alabian Solutions Software company and I live in Ikeja</p>";
+let myFood = "<p>I love to eat beans and fried plantain.</p>";
+
+document.write(myName);
+document.write(myAge);
+document.write(myAddy);
+document.write(myFood);
