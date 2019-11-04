a4236bc3c3e017d69336d8e69a93c5b950ff7d8e
AmucheOgbonna <amuche247cynthia@gmail.com>
2019-11-04 09:54:21 +0100

    Task completed

---
+++ template_literal.js
@@ -0,0 +1,10 @@
+let name = "Bisola";
+let strretNo = 12;
+let streetName ="Calabar Street";
+let State ="Enugu State";
+let age =12;
+let food = "I love fired rice and chicken.";
+let furtherDescription = "I also love to write codes in <strong> Javascript </strong>. Ye, I'm going to be a badass Programmer one day, by God's grace. \n Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do \n eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt <h6>ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat no proident, sunt in culpa qui officia deserunt mollit anim id</h6> est laborum.\nSee you at the top.";
+
+
+document.write(`<h1>Composition</h1>\n My name is${name}, I live in  No ${strretNo} ${streetName},${State}. I'm ${age} years old. ${food} \n ${furtherDescription}`);
