734da7123b8123ee4263ceff5f0de5c4c7d88f4a
ayomidealabi <ayomidealabi2@gmail.com>
2019-11-04 10:37:47 +0100

    second save

---
+++ day1/js/assignment.js
@@ -0,0 +1,14 @@
+let name="Biola"
+let age = "23"
+let food ="beans"
+let address ="12 Oluakerele street"
+document.write(`<h1>Composition</h1>
+	My name is ${name}. <br> 
+	I am ${age} years old. <br>
+	I love eating ${food}. <br>
+	I live at ${address}`);
+
+let x = 4
+let y = 6
+
+console.log(Math.sqrt(x*x + y*y));
