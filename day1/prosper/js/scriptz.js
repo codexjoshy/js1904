722c4398699c33f71e2aacd0ea6c574c5d7793c2
Mp <ikechukwuprosper99@gmail.com>
2019-11-04 08:43:01 -0800

    this is my first commit

---
+++ day1/prosper/js/scriptz.js
@@ -0,0 +1,14 @@
+let name = "prosper";
+let email = "ikechukwuorosper99@gmail.com";
+let number = "0905783499";
+document.write("<h2> MY PERSONAL DETAILS</h2> <p> ")
+
+
+let b = 4;
+let c = 5;
+let a = b**2 + c**2;
+let x = math.sqrt(a);
+console.log(x);
+
+
+
---
+++ day1/prosper/scriptz.js
@@ -0,0 +1,5 @@
+let name = "prosper";
+let email = "ikechukwuorosper99@gmail.com";
+let number = "0905783499";
+document.write ("hello Brainiac")
+
