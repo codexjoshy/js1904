79f106f117ee48ed0ada3d55825a7640e8c7ac60
Mp <ikechukwuprosper99@gmail.com>
2019-11-04 09:44:52 -0800

    this is the new changes

--- day1/prosper/js/scriptz.js
+++ day1/prosper/js/scriptz.js
@@ -1,13 +1,20 @@
 let name = "prosper";
 let email = "ikechukwuorosper99@gmail.com";
 let number = "0905783499";
-document.write("<h2> MY PERSONAL DETAILS</h2> <p> ")
+let address = "no 12 oluakerele stree off balogun bus stop"
+let food = "Bread and egg"
+let hobby = "coding"
+document.write(`<h1>COMPOSITION</h1><br><hr>my name is ${name}
+	  this is my ${email}<br>  and my address is ${address}
+	my phone contact is  ${number} my favourite food is ${food},
+	and what i spend most of time doing is ${hobby}`)
 
 
 let b = 4;
 let c = 5;
 let a = b**2 + c**2;
-let x = math.sqrt(a);
+let x = Math.sqrt(Math.round(a));
+
 console.log(x);
 
 
