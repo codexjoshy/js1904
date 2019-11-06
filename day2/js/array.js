02b18e9e9f9c4878afd534136c04ed0d4cb36be9
joeveloper <bebosir@gmail.com>
2019-11-06 09:34:44 +0100

    Times table

--- day2/js/array.js
+++ day2/js/array.js
@@ -1,5 +1,5 @@
-//array 
-const colors = ['red', 'blue', 'green', 'orange'];
+//array
+// const colors = ['red', 'blue', 'green', 'orange'];
 // console.log(colors[0]);
 // let lengthOfcolors = colors.length;
 // console.log(colors[lengthOfcolors - 1]);
@@ -14,40 +14,52 @@ const colors = ['red', 'blue', 'green', 'orange'];
 // colors.push('yellow');
 // console.log(colors);
 
-
 //DESTRUCTURING
 // const color = ['red', 'blue', 'green', 'orange'];
 // const [getRed, , getGreen, getOrange] = color;
 // console.log(`I love color ${getRed}`);
 
 //spread operators
-const newColor = ["magenta", "brown", "indigo",];
-//array 
-const color = [...newColor ,'red', 'blue', 'green', 'orange'];
+// const newColor = ["magenta", "brown", "indigo",];
+// //array
+// const color = [...newColor ,'red', 'blue', 'green', 'orange'];
 // const newArrayOfColors = [color, ...newColor];
 // console.log(color);
 
 //if statement
-const age = 18;
-let result ="";
-if(age==1 && age <=10){
-    result = 'You are too young to be at the bar';
-}else if(age >10 && age < 17){
-    result = 'Be patient, jus a few more years';
-}else if(age > 40 ){
-    result = 'sorry too old';
-}else{
-    result = 'You are welcome';
-}
+// const age = 18;
+// let result ="";
+// if(age==1 && age <=10){
+//     result = 'You are too young to be at the bar';
+// }else if(age >10 && age < 17){
+//     result = 'Be patient, jus a few more years';
+// }else if(age > 40 ){
+//     result = 'sorry too old';
+// }else{
+//     result = 'You are welcome';
+// }
 // console.log(result);
 
-let enter = (age < 18)? "you are too young" : "You are welcome";
+// let enter = (age < 18)? "you are too young" : "You are welcome";
 
 //loop
 let results = "";
 for (let index = 1; index <= 5; index++) {
-    for(let j = 1; j <= 12; j++){
-        results += ` ${index} X ${j} = ${index * j} <br>`;
-    }
+  for (let j = 1; j <= 12; j++) {
+    results += `
+    <div class="container">
+        <table class="table table-bordered">
+    <tbody>
+      <tr>
+        <td>${index}</td>
+        <td>X</td>
+        <td>${j}</td>
+        <td>=</td>
+        <td>${index * j}</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>`;
+  }
 }
 document.write(results);
